f7792e0bb1db92096a969d021457c465078803a6
doldam0 <jinustar@g.skku.edu>
2020-07-27 21:38:11 +0900

    [Edit] Change preprocessing code

--- util/preprocessing.py
+++ util/preprocessing.py
@@ -4,13 +4,12 @@ import re
 import numpy as np
 
 
-@static_vars(SOT='sot', EOT='eot', PAD=0, _Preprocessor__okt=None)
+@static_vars(SOT='sot', EOT='eot', PAD=0, _Preprocessor__okt=None, _Preprocessor__tokenizer=None)
 class Preprocessor:
     def __init__(self):
         self.__is_hangul_data = False
         self.__sequence_len = 0
         self.__max_words = None
-        self.__tokenizer = None
 
     @staticmethod
     def __is_hangul(text):
@@ -33,7 +32,7 @@ class Preprocessor:
         else:
             def tok(t):
                 sep = [word_tokenize(s) for s in sent_tokenize(t)]
-                return sep if sep_by_sentence else return sum(sep, [])
+                return sep if sep_by_sentence else sum(sep, [])
             try:
                 return tok(text)
             except LookupError:
@@ -60,43 +59,31 @@ class Preprocessor:
         if not isinstance(target, list):
             raise ValueError('The target must be list')
         if isinstance(target[0], list):
-            return [padding(elem) for elem in target]
+            return [self.padding(elem) for elem in target]
         return target + [Preprocessor.PAD] * (self.__sequence_len - len(target))
 
-    def fit_tokenizer(self, sequence):
-        if self.__tokenizer is None:
+    @staticmethod
+    def fit_tokenizer(sequence):
+        if Preprocessor.__tokenizer is None:
             from tensorflow.keras.preprocessing.text import Tokenizer
-            self.__tokenizer = Tokenizer()
-            self.__tokenizer.word_index[Preprocessor.PAD] = Preprocessor.PAD
-            self.__tokenizer.index_word[Preprocessor.PAD] = Preprocessor.PAD
-        self.__tokenizer.fit_on_texts(sequence)
-        return self
+            Preprocessor.__tokenizer = Tokenizer()
+            Preprocessor.__tokenizer.word_index[Preprocessor.PAD] = Preprocessor.PAD
+            Preprocessor.__tokenizer.index_word[Preprocessor.PAD] = Preprocessor.PAD
+        Preprocessor.__tokenizer.fit_on_texts(sequence)
 
     def tokenize(self, sequence):
         result = self.__tokenizer.texts_to_sequences(sequence)
         result.to_numpy = lambda value: np.array(value)
         return result
 
-    def positional_encoding(self, sequence, dim_model, scale=10000):
-        if not isinstance(sequence, np.ndarray):
-            raise ValueError('The sequence must be numpy array')
-        pos = np.arange(self.__sequence_len)[:, np.newaxis]
-        i = np.arange(dim_model)[np.newaxis, :]
-        i[:, 1::2] = i[:, 1::2] - 1
-        encoder = pos / np.power(scale, i / np.float32(dim_model))
-        encoder[:, 0::2] = np.sin(encoder[:, 0::2])
-        encoder[:, 1::2] = np.cos(encoder[:, 1::2])
-        return sequence + encoder
-
-    @property
-    def max_words(self):
+    def get_sequence_len(self):
         return self.__sequence_len
 
-    @property
-    def max_words(self):
-        if self.__max_words is None:
-            raise ValueError("You have to compile your text before get max words")
-        return self.__max_words
+    @staticmethod
+    def get_num_words():
+        if Preprocessor.__tokenizer is None:
+            raise ValueError("You have to compile your text before get the number of the words")
+        return len(Preprocessor.__tokenizer.word_counts) + 1
 
     @property
     def tokenizer(self):
