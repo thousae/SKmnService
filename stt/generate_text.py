d9101be50bebfe33f5886302ad47a13ad0a53d95
Yebin11 <55072785+Yebin11@users.noreply.github.com>
2020-12-23 14:05:11 +0900

    Update generate_text.py

--- stt/generate_text.py
+++ stt/generate_text.py
@@ -13,7 +13,8 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-
+    
+    # upload file to storage
     blob.upload_from_filename(source_file_name)
 
     print(
@@ -37,24 +38,26 @@ def long_running_recognize(storage_uri):
     client = speech.SpeechClient()
 
     # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
-
-    # Encoding of audio data sent. This sample sets this explicitly.
-    # This field is optional for FLAC and WAV audio formats.
     
-    #speech_context = speech.SpeechContext(phrases=[])
-
+    # encoding: audio file format
+    # sample_rate_hertz: audio file hertz
+    # language_code: language of audio. English - en_US, Korean - 'ko_KR'
+    # enable_automatic_punctuation: automatic punctuation
+    
     config = speech.RecognitionConfig(
         encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz = 16000,
         language_code = 'en_US',
         enable_automatic_punctuation = True
-        #,speech_contexts = [speech_context]
     )
 
+    # audio file in storage
     audio = speech.RecognitionAudio(uri = storage_uri)
 
+    # running
     operation = client.long_running_recognize(config = config, audio = audio)
     
+    # take result
     response = operation.result()
     print('stt over')
 
@@ -62,14 +65,27 @@ def long_running_recognize(storage_uri):
 
 
 def generate_text(filepath):
-    json_file_path = os.getcwd() + '/speech-to-text-285502-9513cb3afbc8.json'
+    
+    # json_file_path = your json key file path
+    # bucket_name = name of bucket for uploading file
+    # storage_uri = storage uri for uploading file and running STT 
+    
+    json_file_path = os.getcwd() + '/stt/[json-file-name]'
     bucket_name = 'speechtotext_bin_bucket'
     storage_uri = 'gs://' + bucket_name + '/' + filepath.split("/")[-1]
     
+    # setting
+    # out_file = result file name
+    
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=json_file_path
     out_file = filepath.split(".")[0] + '.txt'
+    
+    # upload file to storage and take result
+    
     upload_blob(bucket_name, filepath, filepath.split("/")[-1])
     response = long_running_recognize(storage_uri)
+    
+    # make txt file
     
     f = open(filepath.split(".")[0] + '.txt', 'w')
     for result in response.results:
